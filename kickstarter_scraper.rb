77af157abfda5ae8d2a5bc4fcf3798ca19650702
Will Andre <wandre94@gmail.com>
2021-02-06 19:30:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,8 @@
-# require libraries/modules here
+require 'nokogiri'
+
+html = File.read('fixtures/kickstarter.html')
+
+kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
   # write your code here
